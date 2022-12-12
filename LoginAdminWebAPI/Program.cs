646f1066a4be367443d4c57a695d448759e34d19
netanel152 <Netanelos152@gmail.com>
2022-12-12 02:31:49 +0200

    Fix issue with Folders

--- LoginAdminWebAPI/Program.cs
+++ LoginAdminWebAPI/Program.cs
@@ -1,14 +1,28 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using LoginAdminWebAPI;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+
 var builder = WebApplication.CreateBuilder(args);
 
+//DB
+builder.Services.AddDbContext<LoginAdminContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("LoginAdminContext")));
+
+
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DI
+ManagersServiceCollection managersOwner = new ManagersServiceCollection(builder.Services);
 
 builder.Services.AddCors(options =>
 {
@@ -18,7 +32,6 @@ builder.Services.AddCors(options =>
                           policy.WithOrigins("http://localhost:3001", "http://localhost:3000").AllowAnyHeader().AllowAnyMethod().WithMethods("PUT", "DELETE", "GET", "POST");
                       });
 });
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +41,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.Use((context, next) =>
+{
+    context.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+    return next.Invoke();
+});
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
@@ -37,4 +58,3 @@ app.MapControllers();
 app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
-
